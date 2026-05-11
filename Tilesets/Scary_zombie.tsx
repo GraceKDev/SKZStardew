947f3f6d506205743b73660a70230ff48ccd3517
thefroggirl <kearnsgracebusiness@gmail.com>
2026-05-11 22:06:07 +1000

    Add new tilesets for jumpscare and zombie hand animations
    - Created 'jumpscare.tmx' with layers for background, buildings, and paths.
    - Created 'zombiehand.tmx' with similar structure and properties.
    - Added 'zombiehand1.tmx' featuring a different path layer with specific tile data.

---
+++ Tilesets/Scary_zombie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Scary_zombie" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="./Scary_zombie.png" width="320" height="320"/>
+</tileset>
